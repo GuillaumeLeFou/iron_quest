65f9dd31f62db297883584118f906bafed9614e2
Guillaume Vanvilthoven <guillaumevvn@gmail.com>
2026-06-27 10:41:38 +0200

    feat: add mock character data

---
+++ mocks/character.ts
@@ -0,0 +1,14 @@
+import { Character } from "@/types/character";
+
+export const mockCharacter: Character = {
+  id: "char-1",
+  accountId: "account-1",
+  name: "PouleGéante",
+  level: 4,
+  xp: 150,
+  gold: 0,
+  strength: 12,
+  endurance: 8,
+  vitality: 10,
+  discipline: 7,
+};
