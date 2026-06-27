f65feacca9b32c2887fcae80cb02ed3a0f44f352
Guillaume Vanvilthoven <guillaumevvn@gmail.com>
2026-06-27 11:43:32 +0200

    feat: display character level and xp bar on home screen

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,9 +1,35 @@
+import { mockCharacter } from "@/mocks/character";
+import { getXpPercentage, getXpRequiredForLevel } from "@/models/progression";
 import { Text, View } from "react-native";
 
 export default function HomeScreen() {
+  const xpPercentage = getXpPercentage(mockCharacter.level, mockCharacter.xp);
+  const xpRequired = getXpRequiredForLevel(mockCharacter.level);
+
   return (
     <View>
-      <Text>Iron Quest - Accueil</Text>
+      <Text> {mockCharacter.name} </Text>
+      <Text> {mockCharacter.level} </Text>
+      <Text>
+        {" "}
+        {mockCharacter.xp} / {xpRequired}{" "}
+      </Text>
+      <View
+        style={{
+          backgroundColor: "#ccc",
+          height: 20,
+          borderRadius: 10,
+          overflow: "hidden",
+        }}
+      >
+        <View
+          style={{
+            backgroundColor: "blue",
+            height: "100%",
+            width: `${xpPercentage}%`,
+          }}
+        />
+      </View>
     </View>
   );
 }
